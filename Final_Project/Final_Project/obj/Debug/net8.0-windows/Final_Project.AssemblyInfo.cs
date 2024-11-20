2fd510aad138b8de7ff05e7ccc5551fc98ac6907
Wayne Edward Miles Bushey IV <wembushey@gmail.com>
2024-11-19 23:51:06 -0500

    started the forms for the main all

--- Final_Project/Final_Project/obj/Debug/net8.0-windows/Final_Project.AssemblyInfo.cs
+++ Final_Project/Final_Project/obj/Debug/net8.0-windows/Final_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Final_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4eecaae17c82016a6eebd51b1b79f17e3e358a24")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c29e0c2ea0af6b348ca6bfb7825b594ad390851")]
 [assembly: System.Reflection.AssemblyProductAttribute("Final_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Final_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Final_Project/Views/Form1.cs
@@ -0,0 +1,15 @@
+namespace Views
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
